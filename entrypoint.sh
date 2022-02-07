ef6b0346cd68be7156724926a4a42cf892142e88
inerject <kumbalup@gmail.com>
2022-02-07 15:37:15 +0200

    protect connection with additional ssh settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
 SSH_SRC_DATA=/root/ssh
 SSH_DEST_DATA=/root/.ssh
 
-SSH_CMD_OPTIONS="-N -R ${REMOTE_HOST}:${REMOTE_PORT}:${LOCAL_HOST}:${LOCAL_PORT}"
+SSH_OPTS="-N"
+SSH_OPTS="${SSH_OPTS} -o ServerAliveCountMax=3"
+SSH_OPTS="${SSH_OPTS} -o ServerAliveInterval=20"
+SSH_OPTS="${SSH_OPTS} -o ExitOnForwardFailure=yes"
+SSH_OPTS="${SSH_OPTS} -R ${REMOTE_HOST}:${REMOTE_PORT}:${LOCAL_HOST}:${LOCAL_PORT}"
 
 if [ ! -z ${IDENTITY_FILE} ]; then
-  SSH_CMD_OPTIONS="-i ${SSH_DEST_DATA}/${IDENTITY_FILE} ${SSH_CMD_OPTIONS}"
+  SSH_OPTS="-i ${SSH_DEST_DATA}/${IDENTITY_FILE} ${SSH_OPTS}"
 fi
 
 rm -rf ${SSH_DEST_DATA} \
   && mkdir ${SSH_DEST_DATA} \
   && cp -R ${SSH_SRC_DATA}/* ${SSH_DEST_DATA}/ \
   && chmod -R 600 ${SSH_DEST_DATA}/* \
-  && ssh ${SSH_CMD_OPTIONS} ${DEST_USER}@${DEST_HOST} \
+  && ssh ${SSH_OPTS} ${DEST_USER}@${DEST_HOST} \
   && while true; do sleep 50; done;
